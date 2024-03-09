ed6eb07b001499e60ef82e6d1cf39e3002f7a81f
Safar Umarov <safarumarov711@gmail.com>
2024-03-10 01:23:06 +0700

    comments for config package

--- config/openapi-generator.php
+++ config/openapi-generator.php
@@ -1,22 +1,24 @@
 <?php
 
 return [
-
+    // The URL where the OpenAPI documentation will be accessible
     'api_docs_url' => public_path('api/v1/index.yaml'),
 
-    // laravel_ddd domain driven develop
+    // The architecture type for the Laravel application
+    // (e.g., "default", "laravel_ddd")
     'architecture_type' => 'default',
 
-    'route_path' => app_path('route/openapi-routes.php'),
-
+    // The entities for which code generation will be performed
     'entities' => [
         'route',
     ],
 
+    // The directory containing stub files used for code generation
     'stubs' => __DIR__.'/../stubs',
 
+    // Additional paths used in the application
     'paths' => [
+        // The file path where the OpenAPI routes will be generated
         'routes_file' => base_path('routes/openapi.php'),
     ],
-
 ];
