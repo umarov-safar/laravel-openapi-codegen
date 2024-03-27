4b808aa1cd6a96132a94723a2fa33568c1561da0
Safar Umarov <safarumarov711@gmail.com>
2024-03-27 17:08:25 +0700

    new name for package

--- src/Console/Commands/OpenapiServerGeneratorCommand.php
+++ src/Console/Commands/OpenapiServerGeneratorCommand.php
@@ -3,12 +3,12 @@
 namespace Openapi\ServerGenerator\Console\Commands;
 
 use cebe\openapi\Reader;
-use Illuminate\Console\GeneratorCommand;
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
 use Openapi\ServerGenerator\Factories\DefaultGeneratorFactory;
 use Openapi\ServerGenerator\Generators\BaseGenerator;
 
-class OpenapiServerGeneratorCommand extends GeneratorCommand
+class OpenapiServerGeneratorCommand extends Command
 {
     protected $signature = 'openapi:generate-server';
 
